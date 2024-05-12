2c85fb0c4460320d3192053f8cfa7d9afec38367
神代綺凛 <i@loli.best>
2024-05-12 19:18:31 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,5 @@
-self.addEventListener('install', () => self.skipWaiting());
-
-self.addEventListener('activate', () => {
-  self.clients.matchAll({ type: 'window' }).then((windowClients) => {
+self.registration.unregister().then(() => {
+  self.clients.matchAll({ type: 'window' }).then(windowClients => {
     for (const windowClient of windowClients) {
       windowClient.navigate(windowClient.url);
     }
